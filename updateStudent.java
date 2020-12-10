12c6cc30967e444f08f1e492601134524680d66d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-10 08:44:50 +0100

    Create updateStudent.java

---
+++ updateStudent.java
@@ -0,0 +1,23 @@
+  public void updateStudent(Student student) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // save the student object
+            String hql = "UPDATE Student set firstName = :firstName " + "WHERE id = :studentId";
+            Query query = session.createQuery(hql);
+            query.setParameter("firstName", student.getFirstName());
+            query.setParameter("studentId", 1);
+            int result = query.executeUpdate();
+            System.out.println("Rows affected: " + result);
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
