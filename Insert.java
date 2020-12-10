09e0ef6c312dec444c5db118c21205f3b2bec722
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-10 08:45:59 +0100

    Create Insert.java

---
+++ Insert.java
@@ -0,0 +1,21 @@
+public void insertStudent() {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            String hql = "INSERT INTO Student (firstName, lastName, email) " +
+                "SELECT firstName, lastName, email FROM Student";
+            Query query = session.createQuery(hql);
+            int result = query.executeUpdate();
+            System.out.println("Rows affected: " + result);
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
